244df39cb7f41e62c5856e00f5f9255389ec3698
Kunal Singh Chauhan <kunalsc12@gmail.com>
2023-07-09 18:30:43 +0530

    This is final version

---
+++ app/src/main/java/com/example/bmibuddy/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.bmibuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val weightText = findViewById<EditText>(R.id.etWeight)
+        val heightText = findViewById<EditText>(R.id.etHeight)
+        val btnCalculate=findViewById<Button>(R.id.btnCalculate)
+        btnCalculate.setOnClickListener {
+            val weight = weightText.text.toString()
+            val height = heightText.text.toString()
+
+            if (invalidInput(weight, height)) {
+                var bmi = weight.toDouble() / ((height.toDouble() / 100) * (height.toDouble() / 100))
+                bmi = String.format("%.2f", bmi).toDouble()
+                displayResult(bmi)
+            }
+        }
+    }
+    private fun invalidInput(weight : String?,height : String?) :Boolean{
+       return when{
+            weight.isNullOrEmpty() ->{
+                Toast.makeText(this,"Empty weight", Toast.LENGTH_SHORT).show()
+                return false
+            }
+            height.isNullOrEmpty() ->{
+                Toast.makeText(this,"Empty height", Toast.LENGTH_SHORT).show()
+                return false
+            }
+            else -> {
+                return true
+            }
+        }
+    }
+    private fun displayResult(bmi : Double){
+        val tvResult=findViewById<TextView>(R.id.tvResult)
+        var tvDetails:TextView=findViewById(R.id.tvDetails)
+        var color =0
+        tvResult.text=bmi.toString()
+        when{
+            bmi<18.50 ->{
+                tvDetails.text="Underweight"
+                color=R.color.resultUnderWeight
+            }
+            bmi in 18.50 ..24.99->{
+                tvDetails.text="Healthy"
+                color=R.color.resultHealthy
+            }
+            bmi in 25.00 ..29.99->{
+                tvDetails.text="Over Weight"
+                color=R.color.resultOverWeight
+            }
+            bmi > 29.99 ->{
+                tvDetails.text="Obese"
+                color=R.color.resultObese
+            }
+
+        }
+        tvDetails.setTextColor(ContextCompat.getColor(this,color))
+    }
+}
