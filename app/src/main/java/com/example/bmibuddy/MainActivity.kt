bd3ff99941b3702a6a62e6a809a73e2b04d2c2ff
Kunal Singh Chauhan <kunalsc12@gmail.com>
2023-07-11 23:11:51 +0530

    icon change

--- app/src/main/java/com/example/bmibuddy/MainActivity.kt
+++ app/src/main/java/com/example/bmibuddy/MainActivity.kt
@@ -16,9 +16,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val weightText = findViewById<EditText>(R.id.etWeight)
         val heightText = findViewById<EditText>(R.id.etHeight)
-        val btnCalculate=findViewById<Button>(R.id.btnCalculate)
-        val imgMale =findViewById<ImageButton>(R.id.imgMale)
-        val imgFemale =findViewById<ImageButton>(R.id.imgFemale)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        val imgMale = findViewById<ImageButton>(R.id.imgMale)
+        val imgFemale = findViewById<ImageButton>(R.id.imgFemale)
         imgMale.setOnClickListener {
             imgMale.setBackgroundColor(Color.RED)
             imgFemale.setBackgroundColor(Color.WHITE)
@@ -33,51 +33,59 @@ class MainActivity : AppCompatActivity() {
             val height = heightText.text.toString()
 
             if (invalidInput(weight, height)) {
-                var bmi = weight.toDouble() / ((height.toDouble() / 100) * (height.toDouble() / 100))
+                var bmi =
+                    weight.toDouble() / ((height.toDouble() / 100) * (height.toDouble() / 100))
                 bmi = String.format("%.2f", bmi).toDouble()
                 displayResult(bmi)
             }
         }
     }
-    private fun invalidInput(weight : String?,height : String?) :Boolean{
-        when{
-            weight.isNullOrEmpty() ->{
-                Toast.makeText(this,"Empty weight", Toast.LENGTH_SHORT).show()
+
+    private fun invalidInput(weight: String?, height: String?): Boolean {
+        when {
+            weight.isNullOrEmpty() -> {
+                Toast.makeText(this, "Empty weight", Toast.LENGTH_SHORT).show()
                 return false
             }
-            height.isNullOrEmpty() ->{
-                Toast.makeText(this,"Empty height", Toast.LENGTH_SHORT).show()
+
+            height.isNullOrEmpty() -> {
+                Toast.makeText(this, "Empty height", Toast.LENGTH_SHORT).show()
                 return false
             }
+
             else -> {
                 return true
             }
         }
     }
-    private fun displayResult(bmi : Double){
-        val tvResult=findViewById<TextView>(R.id.tvResult)
-        var tvDetails:TextView=findViewById(R.id.tvDetails)
-        var color =0
-        tvResult.text=bmi.toString()
-        when{
-            bmi<18.50 ->{
-                tvDetails.text="Underweight"
-                color=R.color.resultUnderWeight
+
+    private fun displayResult(bmi: Double) {
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        var tvDetails: TextView = findViewById(R.id.tvDetails)
+        var color = 0
+        tvResult.text = bmi.toString()
+        when {
+            bmi < 18.50 -> {
+                tvDetails.text = "Underweight"
+                color = R.color.resultUnderWeight
             }
-            bmi in 18.50 ..24.99->{
-                tvDetails.text="Healthy"
-                color=R.color.resultHealthy
+
+            bmi in 18.50..24.99 -> {
+                tvDetails.text = "Healthy"
+                color = R.color.resultHealthy
             }
-            bmi in 25.00 ..29.99->{
-                tvDetails.text="Over Weight"
-                color=R.color.resultOverWeight
+
+            bmi in 25.00..29.99 -> {
+                tvDetails.text = "Over Weight"
+                color = R.color.resultOverWeight
             }
-            bmi > 29.99 ->{
-                tvDetails.text="Obese"
-                color=R.color.resultObese
+
+            bmi > 29.99 -> {
+                tvDetails.text = "Obese"
+                color = R.color.resultObese
             }
 
         }
-        tvDetails.setTextColor(ContextCompat.getColor(this,color))
+        tvDetails.setTextColor(ContextCompat.getColor(this, color))
     }
 }
